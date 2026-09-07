48979fac988d87ba37e563475592db4bb0d0754c
HBB <getmuver3@yandex.ru>
2026-09-07 10:31:53 +0300

    feat: Обеспечить передачу данных между формой и страницей

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,26 +3,73 @@ import { Button } from 'src/ui/button';
 import { Select } from 'src/ui/select';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import clsx from 'clsx';
-import { backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyClasses, fontFamilyOptions, fontSizeOptions, OptionType } from 'src/constants/articleProps';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Separator } from 'src/ui/separator';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
+type ArticleParamsFormProps = {
+	onChange: (articleState: ArticleStateType) => void;
+};
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm = ({ onChange }: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [selectedFont, setSelectedFont] = useState<OptionType>(defaultArticleState.fontFamilyOption);
-	const [selectedFontSize, setSelectedFontSize] = useState<OptionType>(defaultArticleState.fontSizeOption)
-	const [selectedFontColor, setSelectedFontColor] = useState<OptionType>(defaultArticleState.fontColor)
-	const [selectedBackGroundColor, setSelectedBackGroundColor] = useState<OptionType>(defaultArticleState.backgroundColor)
-	const [selectedContentWidth, setSelectedContentWidth] = useState<OptionType>(defaultArticleState.contentWidth)
+	const rootRef = useRef<HTMLDivElement>(null);
+	const [selectedFont, setSelectedFont] = useState<OptionType>(
+		defaultArticleState.fontFamilyOption
+	);
+	const [selectedFontSize, setSelectedFontSize] = useState<OptionType>(
+		defaultArticleState.fontSizeOption
+	);
+	const [selectedFontColor, setSelectedFontColor] = useState<OptionType>(
+		defaultArticleState.fontColor
+	);
+	const [selectedBackGroundColor, setSelectedBackGroundColor] =
+		useState<OptionType>(defaultArticleState.backgroundColor);
+	const [selectedContentWidth, setSelectedContentWidth] = useState<OptionType>(
+		defaultArticleState.contentWidth
+	);
+	const handleSubmit = () =>
+		onChange({
+			fontFamilyOption: selectedFont,
+			fontColor: selectedFontColor,
+			backgroundColor: selectedBackGroundColor,
+			contentWidth: selectedContentWidth,
+			fontSizeOption: selectedFontSize,
+		});
 
+	const handleReset = () => {
+		onChange(defaultArticleState);
+		setSelectedFont(defaultArticleState.fontFamilyOption);
+		setSelectedFontSize(defaultArticleState.fontSizeOption);
+		setSelectedFontColor(defaultArticleState.fontColor);
+		setSelectedBackGroundColor(defaultArticleState.backgroundColor);
+		setSelectedContentWidth(defaultArticleState.contentWidth);
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onChange: setIsOpen,
+	});
 
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
-			<aside className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+			<aside
+				ref={rootRef}
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
 					<h2 className={styles.formTitle}>Задайте параметры</h2>
 					<Select
@@ -58,8 +105,18 @@ export const ArticleParamsForm = () => {
 						onChange={setSelectedContentWidth}
 					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={handleSubmit}
+						/>
 					</div>
 				</form>
 			</aside>
